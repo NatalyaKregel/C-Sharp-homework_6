4b77b80f9b3fb6f779fbe05bb4ab48ebab162320
Nata Kregel <natik.kregel@gmai.com>
2022-09-07 21:43:24 +0300

    Добавили домашнее задание №6

---
+++ homework_6-1/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.WriteLine("Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
+Console.Write("Введите количество чисел M - "); 
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пользователь ввел с клавиатуры: "); 
+int []array = new int[M];
+FillArray(array);
+FindPositiveNumber(array);
+
+// метод заполнения массива
+void FillArray (int[]array)
+{
+    for (int i=0; i < M; i++)
+    {
+        array[i]=new Random().Next(-10,11);
+        Console.Write(array[i]+ ", ");
+    }
+Console.WriteLine();
+}
+
+//Метод нахождения положительных чисел
+void FindPositiveNumber(int[]array)
+{
+    int count = 0;
+    for (int j = 0; j < M; j++)
+    {
+        if (array [j] >= 0)
+        {
+            count++;
+        }        
+    }
+    Console.WriteLine($"Из них {count} чисел являются положительными (т.е.> 0) ");
+}
+Console.WriteLine();
+
+
---
+++ homework_6-2/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.WriteLine("Задача 43: Напишите программу, которая найдёт т.пересечения 2х прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.");
+Console.Write("Введите значения точки b1: "); 
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значения точки k1: "); 
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значения точки b2: "); 
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значения точки k2: "); 
+double k2 = Convert.ToInt32(Console.ReadLine());
+FindPoint (b1, k1, b2, k2);
+
+//метод нахождения точки пересечения 2х прямых
+void FindPoint(double B1, double K1, double B2, double K2)
+{
+    if((k1==k2)&&(b1==b2))
+    {
+        Console.WriteLine ("Данные прямые совпадают");
+    }
+    else if (k1==k2)
+    {
+        Console.WriteLine ("Данные прямые параллельны");
+    }
+    else
+    {
+        double x = (b2-b1)/(k1-k2);
+        double y = (k1*(b2-b1))/(k1-k2)+b1;
+        Console.WriteLine ($"Данные прямые пересекаются, точка их пересечения ({x};{y})");
+    }
+    return;
+}
+Console.WriteLine();
